6df1eb590cc9c908df3017663f551c203030fc74
Shreejal Maharjan <shreejal27@gmail.com>
2023-01-25 12:59:22 +0545

    putting components into their seperate files

---
+++ Tutorial1/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+export default function Header(){
+    return(
+        <div>
+        <nav className="nav">
+        <img src="download.png" className="image"></img>
+        <ul className="navitems">
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+        </nav>
+        </div>
+    )
+}
--- Tutorial1/project1.js
+++ Tutorial1/project1.js
@@ -14,40 +14,11 @@
 //     </div>
 // )
 //we need our jsx to be nested under a single parent element. Here div is the parent element for h1 and ul.
-function Header(){
-    return(
-        <div>
-        <nav className="nav">
-        <img src="download.png" className="image"></img>
-        <ul className="navitems">
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-        </nav>
-        </div>
-    )
-}
-function Info(){
-    return (
-        <div>
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordn Walke</li>
-            <li> Has Well over 100k stars on Github</li>
-            <li>Is maintained by Facebook</li>
-        </ul>
-        </div>
-    )
-}
-function Footer(){
-    return(
-        <p>
-            2023 React Development. All right reserved.
-        </p>
-    )
-}
+
+import Header from "./Header"
+import Header from "./Footer"
+import Info from "./Info"
+
 
 function Page(){
     return(
@@ -58,7 +29,6 @@ function Page(){
         </div>
     )
 }
-
 // ReactDOM.render(<Header/>, document.getElementById("header"));
 ReactDOM.render(<Page/>, document.getElementById("root"));
 // ReactDOM.render(<Footer/>, document.getElementById("footer"));
