9a54fb592ead8a779dd1d4c837f18a2d319d9d66
Shreejal Maharjan <shreejal27@gmail.com>
2023-02-13 20:45:57 +0545

    static card rated

--- airbnb_site/src/component/card.js
+++ airbnb_site/src/component/card.js
@@ -1,14 +1,16 @@
 import React from "react"
 import bebe from "../image/bebe.jpg"
 import star from "../image/star.png"
-export default function Hero(){
+export default function Card(){
     return(
     
         <div >
         <img src={bebe} className="cardImage"></img>
         <div className="cardElement">
        <img src={star} className="star"></img>
-        <span className="rating">5.0</span>
+        <span>5.0</span> 
+        <span> (99)</span> 
+        <span> USA</span> 
         <p>
             Live singing concert with Bebe Rexha
         </p>
