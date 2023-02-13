89cd2b02804d86439114e953f4ea60ed54e9b82d
Shreejal Maharjan <shreejal27@gmail.com>
2023-02-13 23:49:51 +0545

    beginning props

--- airbnb_site/src/index.js
+++ airbnb_site/src/index.js
@@ -1,6 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import App from "./App"
+// import App from "./App"
 
 
+function App(){
+    const firstName="Shree";
+    const lastName="Jal"
+    return(<h1> Hello {firstName} {lastName}</h1>);
+}
+// ReactDOM.render(<App/>, document.getElementById("root"));
 ReactDOM.render(<App/>, document.getElementById("root"));
