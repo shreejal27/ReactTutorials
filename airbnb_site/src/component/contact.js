f19f42666fc6c7b2dbe9f3218ddec21872d11d62
Shreejal Maharjan <shreejal27@gmail.com>
2023-02-16 18:49:24 +0545

    restructuring props

--- airbnb_site/src/component/contact.js
+++ airbnb_site/src/component/contact.js
@@ -1,17 +1,17 @@
 import React from "react"
 import star from "../image/star.png"
 import bebe from "../image/bebe.jpg"
-export default function Contact(props){
-    console.log(props.img)
+export default function Contact({img,name,email}){
+
     return(
         <div >
-        <img src={props.img} className="cardImage"></img>
-        <h3 className="cardTitle">{props.name}</h3>
+        <img src= {img} className="cardImage"></img>
+        <h3 className="cardTitle">{name}</h3>
         <div className="cardElement">
        <img src={star} className="star"></img>
         <span>5.0</span> 
         <p>
-        {props.email}
+        {email}
         </p>
         </div>
         </div>
