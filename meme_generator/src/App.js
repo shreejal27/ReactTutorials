f4593cbf999d395d7c955be4d85545c088640fba
Shreejal Maharjan <shreejal27@gmail.com>
2023-02-23 16:02:10 +0545

    made a static form

--- meme_generator/src/App.js
+++ meme_generator/src/App.js
@@ -3,6 +3,7 @@ import "./App.css"
 
 
 import Header from "./components/header"
+import Form from "./components/form"
 
 
 export default function Page(){
@@ -10,6 +11,7 @@ export default function Page(){
     return(
         <div> 
        <Header/>
+       <Form/>
         </div>
     )
 }
---
+++ meme_generator/src/components/form.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+export default function Form(){
+    return(
+        <div>
+            <center>
+                
+        <form>
+            <input type="text" className="formText"/>
+            <input type="text" className="formText"/>
+            <br/>
+            <input type="submit" value="Get a new Meme Image"/>
+        </form>
+            </center>
+        </div>
+    )
+}
