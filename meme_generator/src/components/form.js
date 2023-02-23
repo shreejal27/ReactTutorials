3beaccede60df7fbf9c17dd7acc69e994dfcbfb9
Shreejal Maharjan <shreejal27@gmail.com>
2023-02-23 17:26:50 +0545

    added memedata and fetched imageUrl

--- meme_generator/src/components/form.js
+++ meme_generator/src/components/form.js
@@ -1,16 +1,22 @@
 import React from "react"
-
+import memesData from "../memeData.js"
 
 export default function Form(){
+    function getMemeImage(){
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random()* memesArray.length)
+        const url= memesArray[randomNumber].url
+        alert(url);
+    }
     return(
         <div>
             <center>
                 
         <form>
-            <input type="text" className="formText"/>
-            <input type="text" className="formText"/>
+            <input type="text" /> 
+            <input type="text" />
             <br/>
-            <input type="submit" value="Get a new Meme Image"/>
+            <button onClick={getMemeImage}>Get a new Meme Image</button>
         </form>
             </center>
         </div>
