e14e1be36f3f9d6c33cd9b542ef49a9c7a36201b
Shreejal Maharjan <shreejal27@gmail.com>
2023-11-08 22:45:04 +0545

    learning state

--- meme_generator/src/index.js
+++ meme_generator/src/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+
+
+function App() {
+    const [things, setThings] = React.useState(["Thing 1", "Thing 2"])
+
+    // const thingsArray = ["thing1", "thing2", "thing3"]
+    //this line will run only one time and will not change
+
+    function addItem(){
+        // thingsArray.push("thing" + (thingsArray.length + 1));
+        const newItem = `Thing ${things.length + 1}`;
+        // thingsArray.push(newItem);
+        setThings(prevState => [...prevState, newItem])
+        // console.log(thingsArray);
+        
+    }
+    const thingsElements = things.map(thing => <p key={thing}>{thing}</p>);
+
+    // console.log(thingsArray);
+    return (
+        <div>
+            <button onClick={addItem}> Add Item</button>
+            {thingsElements}
+        </div>
+    )
+}
 
 
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+
+ReactDOM.render(<App />, document.getElementById("root"));
