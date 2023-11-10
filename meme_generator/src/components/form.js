54b1fce2f656b4aa7aecba96f5a9dccf87e58616
Shreejal Maharjan <shreejal27@gmail.com>
2023-11-10 21:44:12 +0545

    converted into object in memegeneraor

--- meme_generator/src/components/form.js
+++ meme_generator/src/components/form.js
@@ -2,13 +2,25 @@ import React from "react"
 import memesData from "../memeData.js"
 
 export default function Form() {
-    const [memeImage, setMemeImage] = React.useState("")
+    const [memeImage, setMemeImage] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "",
+    })
+
+    // const[allMemeImages, setAllMemeImages] = React.useState(memesData)
 
     function getMemeImage() {
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        // const url = memesArray[randomNumber].url
-        setMemeImage(memesArray[randomNumber].url)
+        const url = memesArray[randomNumber].url
+        // setMemeImage(memesArray[randomNumber].url)
+        setMemeImage(prevImage => {
+            return{
+              ...prevImage,
+              randomImage: url
+            }
+          })
     }
     return (
         <div>
@@ -19,7 +31,7 @@ export default function Form() {
                     <br />
                     <button onClick={getMemeImage}>Get a new Meme Image</button>
                 </div>
-                <img src={memeImage} className="meme--Image" alt="meme" />
+                <img src={memeImage.randomImage} className="meme--Image" alt="meme" />
             </center>
 
         </div>
